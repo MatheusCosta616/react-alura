b2f2f814321d898dc7d48a06a3c78379bb66e0f3
MatheusCosta616 <rm551157@fiap.com.br>
2023-03-22 14:49:35 -0300

    Aula 1 completa

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 
 import './App.css';
 import Banner from './componentes/Banner/Banner.js';
-import CampoTexto from './componentes/Banner/CampoTexto/CampoTexto';
 import Formulario from './componentes/Banner/Formulario';
 
 function App() {
   return (
     <div className="App">
-      <Banner/>
-      <Formulario/>
+      <Banner />
+      <Formulario />
     </div>
-    
+
   );
 }
 
