83aa86648768ea56eee520cd45903c2a5052b76d
MatheusCosta616 <rm551157@fiap.com.br>
2023-03-22 13:53:41 -0300

    Upado no github

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,13 @@
+import './Banner.css'
+const Banner = () => {
+    return(
+        <header className='banner'>
+        <img src="./img/imagens/banner.png" alt="O Banner principal do projeto"/>
+        </header>
+    )
+}
+
+
+export default Banner
+
+
---
+++ src/componentes/Banner/CampoTexto/CampoTexto.js
@@ -0,0 +1,13 @@
+import './CampoTexto.css'
+
+const CampoTexto = () => {
+    return (
+        <div className = "campo-texto"> 
+        <label></label>
+        <input placeholder='Digite seu nome' />
+        </div>
+
+    )
+}
+
+export default CampoTexto
